b50d85024e568436852057e7e58669c6109a541a
Danis <firefoxdest@gmail.com>
2020-08-22 14:59:13 +0500

    add  bubble chart

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,33 @@
-df
+new Chart(document.getElementById("bubble-chart"), {
+    type: 'bubble',
+    data: {
+      datasets: [{
+          backgroundColor: "rgba(193,46,12,0.2)",
+          borderColor: "rgba(193,46,12,1)",
+          data: [{
+            x: 200,
+            y: 4,
+            r: 15
+          }]
+        }
+      ]
+    },
+    options: {
+     scales: {
+        yAxes: [{ 
+          scaleLabel: {
+            labelString: "Минуты",
+            min : 200,
+            max : 1000,
+          }
+        }],
+        xAxes: [{ 
+          scaleLabel: {
+            labelString: "Интернет",
+            min : 4,
+            max : 40,
+          }
+        }]
+      }
+    }
+});
